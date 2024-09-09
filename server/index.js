f18d603a0ce96a141d6d6bc10c274e0546b0aab3
hyunho4532 <kyounguk192@gmail.com>
2024-09-09 16:52:47 +0900

    Dev . current location marker select success

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/reverse/geocode", async (req, res) => {
+
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+
+    try {
+        const response = await axios.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc', {
+          headers: {
+            'X-NCP-APIGW-API-KEY-ID': '6dqjmlyppv',
+            'X-NCP-APIGW-API-KEY': 'BRR4QIDkcd9Ilj2uos8MNv214t7a5dJioSZ3HZXc'
+          },
+          params: {
+            coords: `${longitude},${latitude}`,
+            orders: 'roadaddr',
+            output: 'json'
+          }
+        });
+        res.json(response.data);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from Naver API' });
+      }
+})
+
+app.listen(3000, (req, res) => {
+    console.log("3000번 포트 연결 완료");
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { css } from '@emotion/css';
-import { useEffect, useMemo, useRef } from 'react';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [map, setMap] = useState();
+  const [address, setAddress] = useState();
   const mapRef = useRef(null);
 
-  const initMap = useEffect(() => {
+  useEffect(() => {
     const map = new window.naver.maps.Map(mapRef.current, {
       center: new window.naver.maps.LatLng(37.3595704, 127.105399),
       zoom: 10,
     })
 
-    initMap
+    setMap(map);
   }, []);
 
   const currentLocation = () => {
@@ -19,16 +22,38 @@ function App() {
         (position) => {
           const { latitude, longitude } = position.coords;
 
-          
+          const currentLocationMarker = new window.naver.maps.Marker({
+            position: new naver.maps.LatLng(latitude, longitude),
+            map: map
+          })
+
+          axios.post("http://localhost:3000/reverse/geocode", { 
+            latitude: latitude,
+            longitude: longitude
+          })
+            .then(response => {
+              console.log(response.data.results[0])
+
+              setAddress(
+                response.data.results[0].region.area1.name +
+                response.data.results[0].region.area2.name +
+                response.data.results[0].land.name +
+                response.data.results[0].land.number1);
+          })
+            .catch(error => {
+              console.error('Error in reverse geocoding request:', error);
+          });
         }
       )
     }
   }
 
 
+
   return (
     <div className={css`
       text-align: center;
+      margin: 0 auto;
     `}>
       <div className={css`
         display: flex;
@@ -40,15 +65,19 @@ function App() {
           width: 160px;
           height: 40px;
           margin-left: 16px;
-        `}>현재 위치 가져오기</button>
+        `} onClick={() => currentLocation()}>현재 위치 가져오기</button>
         
         <button className={css`
           width: 160px;
           height: 40px;
           margin-left: 16px;
         `}>위치 지정하기</button>
-
       </div>
+
+      <p className={css`
+          text-align: left;
+      `}>{address}</p>
+
       <div className={css`
         width: 1080px;
         height: 620px;
